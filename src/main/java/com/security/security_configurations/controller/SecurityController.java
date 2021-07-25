504cd898e6b7663aa50e497cdac24f049fcb7a07
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-07-25 15:57:05 +0100

    inMemoryAuthentication

---
+++ src/main/java/com/security/security_configurations/controller/SecurityController.java
@@ -0,0 +1,22 @@
+package com.security.security_configurations.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityController {
+    @GetMapping("/")
+    public String itWorks(){
+        return "It works";
+    }
+    @GetMapping("/admin")
+    public String itWorksAdmin(){
+        return "It works admin";
+    }
+    @GetMapping("/user")
+    public String itWorksUser(){
+        return "It works users";
+    }
+}
---
+++ src/test/java/com/security/security_configurations/SecurityConfigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.security_configurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConfigurationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
